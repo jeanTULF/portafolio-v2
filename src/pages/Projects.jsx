9af4d51d38b42f4259685b8fe2616d4f4e761aea
Jean <jeanpolo15@gmail.com>
2024-04-06 22:13:55 -0500

    fix: ruta e imagenes (vercel)

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -9,7 +9,7 @@ export const Projects = () => {
                 <div className="mt-16 flex justify-center">
                     <div className="grid grid-cols-1 gap-14 lg:grid-cols-2">
                         <Cards 
-                            img="/assets/images/minuatura-hiupy.png" 
+                            img={"/assets/images/minuatura-hiupy.png"}
                             alt="hiupy"
                             title="Hiupy"
                             description="Hiupy es una aplicación de inventario y planificación alimentaria."
@@ -17,28 +17,28 @@ export const Projects = () => {
                         />
 
                         <Cards 
-                            img="/assets/images/miniatura-org.png" 
+                            img={"/assets/images/miniatura-org.png"}
                             alt="org project"
                             title="Org"
                             description="Hiupy es una aplicación de inventario y planificación alimentaria."
                         />
 
                         <Cards 
-                            img="/assets/images/miniatura-alurageek.png" 
+                            img={"/assets/images/miniatura-alurageek.png"} 
                             alt="alurageek"
                             title="AluraGeek"
                             description="Hiupy es una aplicación de inventario y planificación alimentaria."
                         />
 
                         <Cards 
-                            img="/assets/images/miniatura-portafolio.png" 
+                            img={"/assets/images/miniatura-portafolio.png"} 
                             alt="portafolio" 
                             title="Portafolio V1"
                             description="Hiupy es una aplicación de inventario y planificación alimentaria."
                         />
 
                         <Cards 
-                            img="/assets/images/miniatura-encriptador.png" 
+                            img={"/assets/images/miniatura-encriptador.png"} 
                             alt="encriptador"
                             title="Encriptador"
                             description="Hiupy es una aplicación de inventario y planificación alimentaria."
